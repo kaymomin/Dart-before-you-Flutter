8744b63804eb679e2bc1c76ffaeeb433f1b5bdf6
Krinza Momin <44579545+kaymomin@users.noreply.github.com>
2020-01-04 11:06:36 +0500

    Create StringInterpolation.dart

---
+++ StringInterpolation.dart
@@ -0,0 +1,13 @@
+main() {
+  
+String coolkid = "Dart";
+print("$coolkid is the cool kid");
+
+print("One plus one is ${1+1}, basic Maths!");
+  
+var Concatenation = 'String '
+    'concatenation'
+    "works even over line breaks.";
+print(Concatenation);
+
+}
