85d0a547e21df640a57e5cc0b3c61b1fe8fe9e2c
Krinza Momin <44579545+kaymomin@users.noreply.github.com>
2020-01-04 11:21:39 +0500

    Create dynamic.dart

---
+++ dynamic.dart
@@ -0,0 +1,15 @@
+main() {
+  
+dynamic dynamicVar = "I\'m a String";
+print (dynamicVar);
+print (dynamicVar.runtimeType);
+
+dynamicVar = 100;
+print (dynamicVar);
+print (dynamicVar.runtimeType);
+  
+dynamicVar = true;
+print (dynamicVar);
+print (dynamicVar.runtimeType);
+
+}
