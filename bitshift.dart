c62e0bf93001703550382bb2259e84ba9fc09d4e
Krinza Momin <44579545+kaymomin@users.noreply.github.com>
2020-01-04 13:15:01 +0500

    Create bitshift.dart

---
+++ bitshift.dart
@@ -0,0 +1,11 @@
+main() {
+  var Op1 = 5;
+  var Op2 = 4;
+
+  print(Op1 & Op2); // AND
+  print(Op1 | Op2); // OR
+  print(Op1 ^ Op2); // XOR
+  print(Op2 << 4); // Op2 Shift Left 4
+  print(Op1 >> 3); // Op1 Shift Right 3
+
+}
