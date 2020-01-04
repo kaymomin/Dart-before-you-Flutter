a5f28b4d9b1cdd19a10880356ddb49ec53303688
Krinza Momin <44579545+kaymomin@users.noreply.github.com>
2020-01-04 12:23:32 +0500

    Create relationalEqu.dart

---
+++ relationalEqu.dart
@@ -0,0 +1,12 @@
+main() {
+var operand1 = 5;
+var operand2 = 3;
+//relational
+print(operand1 > operand2);
+print(operand1 < operand2);
+print(operand1 >= operand2);
+print(operand1 <= operand2);
+//equality
+print(operand1 == operand2);
+print(operand1 != operand2);
+}
