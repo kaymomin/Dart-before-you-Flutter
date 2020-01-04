1e8c4197f1dc596631dd45fcc3ae9579c31d400a
Krinza Momin <44579545+kaymomin@users.noreply.github.com>
2020-01-04 10:43:32 +0500

    Update dataVar.dart

--- dataVar.dart
+++ dataVar.dart
@@ -4,7 +4,7 @@ double secondDartVar = 2.97;
 String myString = "Cool kid in the house!";
 bool logic = true;
 List myList = [1, 3.12, "Dart, dart is the cool kid"];
-Map myMap = {"Kid" : "Dart", "Level" : "Out of this world");
+Map myMap = {"Kid" : "Dart", "Level" : "Out of this world"};
 int UnInitializedVariable;
 
 print(firstDartVar);
