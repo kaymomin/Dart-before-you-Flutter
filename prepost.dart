02ba9daad5e237eecad2bce4606c1c72749a5a36
Krinza Momin <44579545+kaymomin@users.noreply.github.com>
2020-01-04 12:10:17 +0500

    Create prepost.dart

---
+++ prepost.dart
@@ -0,0 +1,16 @@
+
+
+main() {
+  
+int pre = 7;
+int post = 5;
+print(++pre); //Prefix Increment
+print(post++); //Postfix Increment
+print(post); //Postfix Increment
+print(--pre); //prefix Decrement
+print(post--); //postfix Decrement
+print(post); //postfix Decrement
+
+}
+
+
