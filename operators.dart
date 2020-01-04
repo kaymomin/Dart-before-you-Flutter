ef5e851612b00c2712380fb44bf4adb5cd285584
Krinza Momin <44579545+kaymomin@users.noreply.github.com>
2020-01-04 11:34:25 +0500

    Create operators.dart

---
+++ operators.dart
@@ -0,0 +1,12 @@
+main() {
+  var operand1 = 7;
+  var operand2 = 4.5;
+  
+  print (operand1 + operand2); //Addition operator.
+  print (operand1 - operand2); //Subtraction operator.
+  print ( - operand1); //Return the negative value of this integer.
+  print (operand1 * operand2); //Multiplication operator.
+  print (operand1 / operand2); //Division operator.
+  print (operand1 ~/ operand2); //Truncating division operator.
+  print (operand1 % operand2); //Euclidean modulo operator.
+}
